ce646a189c129e525728413d5e7e240cb72e13b2
BilalChbanat <bilal.chbanat2003@gmail.com>
2025-03-13 22:13:08 +0000

    app config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {LangingComponent} from './pages/langing/langing.component';
+import {RegisterComponent} from './auth/register/register.component';
 
 export const routes: Routes = [
-  {path: '' , component : LangingComponent}
+  {path: '' , component : LangingComponent},
+  {path: 'signup', component : RegisterComponent},
 ];
