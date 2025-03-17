7006ffa2982273c3fc6a2999b87b05af7c3b0647
BilalChbanat <bilal.chbanat2003@gmail.com>
2025-03-17 10:01:48 +0000

    route

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
 import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { routes } from './app.routes';
 import {HttpClientModule} from '@angular/common/http';
+import {AuthInterceptor} from './interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), importProvidersFrom(HttpClientModule)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes), importProvidersFrom(HttpClientModule),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import {LangingComponent} from './pages/langing/langing.component';
 import {RegisterComponent} from './auth/register/register.component';
 import {LoginComponent} from './auth/login/login.component';
 import {ProfileComponent} from './auth/profile/profile.component';
+import {DepartmentComponent} from './department/department.component';
 
 export const routes: Routes = [
   {path: '', component: LangingComponent},
   {path: 'signup', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
   {path: 'profile', component: ProfileComponent},
+  { path: 'departments', component: DepartmentComponent },
 ];
