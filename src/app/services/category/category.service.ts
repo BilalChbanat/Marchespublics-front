44936c361113376a6fea34df9ab8a2419242a32a
BilalChbanat <bilal.chbanat2003@gmail.com>
2025-03-19 00:54:35 +0000

    category done

---
+++ src/app/services/category/category.service.ts
@@ -0,0 +1,53 @@
+// src/app/services/category.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface PageResponse<T> {
+  content: T[];
+  pageable: any;
+  last: boolean;
+  totalPages: number;
+  totalElements: number;
+  size: number;
+  number: number;
+  sort: any;
+  first: boolean;
+  numberOfElements: number;
+  empty: boolean;
+}
+
+export interface Category {
+  id: number;
+  name: string;
+  description?: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoryService {
+  private apiUrl = 'http://localhost:8080/categories';
+
+  constructor(private http: HttpClient) { }
+
+  getAllCategories(): Observable<PageResponse<Category>> {
+    return this.http.get<PageResponse<Category>>(this.apiUrl);
+  }
+
+  getCategoryById(id: number): Observable<Category> {
+    return this.http.get<Category>(`${this.apiUrl}/${id}`);
+  }
+
+  createCategory(category: Category): Observable<Category> {
+    return this.http.post<Category>(this.apiUrl, category);
+  }
+
+  updateCategory(id: number, category: Category): Observable<Category> {
+    return this.http.put<Category>(`${this.apiUrl}/${id}`, category);
+  }
+
+  deleteCategory(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
